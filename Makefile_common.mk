1b5f5b77156483cd0614c5d3f722de89140e9f54
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-07-14 13:48:24 +0200

    Add common vars for autotools

---
+++ Makefile_common.mk
@@ -0,0 +1,16 @@
+AUTOTOOLS_CLEAN = \
+Makefile.in \
+aclocal.m4 \
+build-stamp \
+config.guess \
+config.h \
+config.h.in \
+config.sub \
+configure \
+configure-stamp \
+depcomp \
+install-sh \
+ltconfig \
+ltmain.sh \
+missing mkinstalldirs \
+stamp-h.in
